8c88ff7edeb5adf9e0c665f6cbaf43e436c6b4e9
vkshsingh007 <vkshsingh007@gmail.com>
2025-02-27 22:45:07 +0530

    making all component for job-card,tag,section

---
+++ resources/views/components/job-card.blade.php
@@ -0,0 +1,15 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <div class="self-start text-sm">Laracast</div>
+    <div class="py-8 font-bold">
+        <h3>Video Producer</h3>
+        <p>Full Time - from $60,000</p>
+    </div>
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+            <x-tag>Tag</x-tag>
+        </div>
+        <img class="rounded-xl" src="http://placehold.it/42/42" alt="">
+    </div>
+</div>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Pixel Position</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
@@ -24,7 +24,7 @@
             <div><a href="">Post a Job</a></div>
         </nav>
 
-        <main class="mt-10 max-w-[986px]">
+        <main class="mt-10 max-w-[986px] mx-auto">
             {{ $slot }}
         </main>
     </div>
